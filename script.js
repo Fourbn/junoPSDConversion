3018db670bd09a402719c2e2665abf8268598e9a
Philip Turkiewicz <philip.turkiewicz@live.ca>
2020-07-13 23:44:56 -0400

    responsive queries done, almost done!

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Toggle some classes to make the mobile nav button work
+$(function () {
+   $(".fa-bars").on("click", function () {
+      $("nav ul").toggleClass("nav-slide-in");
+      $(".fa-bars").toggleClass("bars-slide");
+   })
+});
